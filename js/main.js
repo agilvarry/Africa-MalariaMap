158e6d71dd4518cb8b077330e8edc8f36ae67660
agilvarry <gilvarry@wisc.edu>
2018-03-21 22:04:09 -0600

    Updated Styles and popups
    Updated Styles and popups

--- js/main.js
+++ js/main.js
@@ -2,47 +2,54 @@
 //self calling global function
 (function(){
   const attributes = ['malariaCases', 'allMalariaDeaths', 'deathsUnderFive', 'bedNets', 'treatment'];
-  let expressed = attributes[3]; //initial attribute
+  let expressed = attributes[4]; //initial attribute
   const attrFull = {
     malariaCases : 'Notified cases of malaria per 100,000 people',
     allMalariaDeaths : 'Malaria deaths per 100,000 people',
     deathsUnderFive : 'Malaria death rate per 100,000 people under 5',
-    bedNets : 'Percent under 5 sleeping under insecticide-treated bed nets',
-    treatment : 'Percent under 5 with fever being treated with anti-malarial drugs'
+    bedNets : 'Percent under 5 with insecticide-treated bed nets',
+    treatment : 'Percent under 5 with fever recieving anti-malarial drugs'
+  }
+  const attrPop = {
+    malariaCases : 'Malaira Cases',
+    allMalariaDeaths : 'Malaria Deaths',
+    deathsUnderFive : 'Child Deaths',
+    bedNets : 'Children with Bed Nets',
+    treatment : 'Children Being Treated'
   }
 
   //chart frame dimensions
   const chartWidth = window.innerWidth * 0.5,
-      chartHeight = 460,
-      leftPadding = 40,
-      rightPadding = 2,
-      topBottomPadding = 5,
-      chartInnerWidth = chartWidth - leftPadding - rightPadding,
-      chartInnerHeight = chartHeight - topBottomPadding * 2,
-      translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
-//need to dynamically update yScale domain
-      let yScale = d3.scaleLinear()
-          .range([chartHeight-10,0])
-          .domain([0, 100]);
-       yAxis = d3.axisLeft(yScale);
+  chartHeight = 460,
+  leftPadding = 40,
+  rightPadding = 2,
+  topBottomPadding = 5,
+  chartInnerWidth = chartWidth - leftPadding - rightPadding,
+  chartInnerHeight = chartHeight - topBottomPadding * 2,
+  translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+  //need to dynamically update yScale domain
+  let yScale = d3.scaleLinear()
+  .range([chartHeight-10,0])
+  .domain([0, 100]);
+  yAxis = d3.axisLeft(yScale);
   //start script when window loads
   window.onload = setMap();
 
   function setSVG(africanCountries, map, path, colorScale){
-      const countries = map.selectAll('countries')
-      .data(africanCountries)
-      .enter()
-      .append('path')
-      .attr('d', path)
-      .attr('class', d=> 'countries '+ "co" + d.properties.Code)
-      .style("fill", d=>choropleth(d.properties, colorScale))
-      .on("mouseover", d => highlight(d.properties))
-      .on("mouseout", d => dehighlight(d.properties))
-      .on("mousemove", moveLabel);
+    const countries = map.selectAll('countries')
+    .data(africanCountries)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('class', d=> 'countries '+ "co" + d.properties.Code)
+    .style("fill", d=>choropleth(d.properties, colorScale))
+    .on("mouseover", d => highlight(d.properties))
+    .on("mouseout", d => dehighlight(d.properties))
+    .on("mousemove", moveLabel);
 
-      let desc = countries.append("desc")
+    let desc = countries.append("desc")
     .text('{"stroke": "#000", "stroke-width": "0.5px"}');
-    };
+  };
 
   // function setGraticule(map, path){
   //     //...GRATICULE BLOCKS FROM PREVIOUS MODULE
@@ -59,37 +66,37 @@
   //     .attr("d", path); //project graticule line
   // };
   function joinData(africanCountries, malaria){
-      //...DATA JOIN LOOPS FROM EXAMPLE 1.1
-      //loop through csv to assign each set of csv attribute values to geojson region
-      for (let i=0; i<malaria.length; i++){
-        let csvCountry = malaria[i]; //the current country in the CVS
-        let csvCode = csvCountry.Code; //the code of the current country in the CSV
+    //...DATA JOIN LOOPS FROM EXAMPLE 1.1
+    //loop through csv to assign each set of csv attribute values to geojson region
+    for (let i=0; i<malaria.length; i++){
+      let csvCountry = malaria[i]; //the current country in the CVS
+      let csvCode = csvCountry.Code; //the code of the current country in the CSV
 
-        //loop through geojson regions to find correct region
-        for (let j=0; j<africanCountries.length; j++){
+      //loop through geojson regions to find correct region
+      for (let j=0; j<africanCountries.length; j++){
 
-          let jsonCountry = africanCountries[j].properties; //the current region geojson properties
-          let jsonCode = jsonCountry.Code; //the geojson primary key code
+        let jsonCountry = africanCountries[j].properties; //the current region geojson properties
+        let jsonCode = jsonCountry.Code; //the geojson primary key code
 
-          //where primary keys match, transfer csv data to geojson properties object
-          if (jsonCode == csvCode){
-            //assign all attributes and values
-            attributes.forEach(function(a){
-              let val = parseFloat(csvCountry[a]); //get csv attribute value
+        //where primary keys match, transfer csv data to geojson properties object
+        if (jsonCode == csvCode){
+          //assign all attributes and values
+          attributes.forEach(function(a){
+            let val = parseFloat(csvCountry[a]); //get csv attribute value
 
-              jsonCountry[a] = val; //assign attribute and value to geojson properties
+            jsonCountry[a] = val; //assign attribute and value to geojson properties
 
-            });
-          };
+          });
         };
       };
+    };
 
-      return africanCountries;
+    return africanCountries;
   };
 
   //function to create color scale generator
-function makeColorScale(data){
-    const colorClasses = ["#D4B9DA", "#C994C7","#DF65B0","#DD1C77","#980043"];
+  function makeColorScale(data){
+    const colorClasses = ["#edf8e9", "#bae4b3","#74c476","#31a354","#006d2c"];
 
     //create color scale generator
     const colorScale = d3.scaleQuantile().range(colorClasses);
@@ -97,8 +104,8 @@ function makeColorScale(data){
     //build array of all values of the expressed attribute
     let domainArray = [];
     for (let i=0; i<data.length; i++){
-        let val = parseFloat(data[i][expressed]);
-        domainArray.push(val);
+      let val = parseFloat(data[i][expressed]);
+      domainArray.push(val);
 
     };
 
@@ -106,104 +113,104 @@ function makeColorScale(data){
     colorScale.domain(domainArray);
 
     return colorScale;
-};
+  };
 
-//function to test for data value and return color
-function choropleth(props, colorScale){
+  //function to test for data value and return color
+  function choropleth(props, colorScale){
     //make sure attribute value is a number
     let val = parseFloat(props[expressed]);
     //if attribute value exists, assign a color; otherwise assign gray
     if (typeof val == 'number' && !isNaN(val)){
-        return colorScale(val);
+      return colorScale(val);
     } else {
-        return "#CCC";
+      return "#CCC";
     };
-};
+  };
 
-function setChart(malaria, colorScale){
+  function setChart(malaria, colorScale){
 
-const fraction = chartWidth / malaria.length;
+    const fraction = chartWidth / malaria.length-10;
     //create a second svg element to hold the bar chart
     //create vertical axis generator
-   //yAxis = d3.axisLeft(yScale);
+    //yAxis = d3.axisLeft(yScale);
 
+    const chart = d3.select("#graph")
+    .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
+    .attr("class", "chart");
 
-    const chart = d3.select("body")
-        .append("svg")
-        .attr("width", chartWidth)
-        .attr("height", chartHeight)
-        .attr("class", "chart");
+    // const chartBackground = chart.append("rect")
+    // .attr("class", "chartBackground")
+    // .attr("width", chartInnerWidth)
+    // .attr("height", chartInnerHeight)
+    // .attr("transform", translate);
 
-        const chartBackground = chart.append("rect")
-        .attr("class", "chartBackground")
-        .attr("width", chartInnerWidth)
-        .attr("height", chartInnerHeight)
-        .attr("transform", translate);
+    var axis = chart.append("g")
+    .attr("class", "axis")
+    .attr("transform", translate)
+    .call(yAxis);
 
-        var axis = chart.append("g")
-               .attr("class", "axis")
-               .attr("transform", translate)
-               .call(yAxis);
+    //  //create frame for chart border
+    // const chartFrame = chart.append("rect")
+    //     .attr("class", "chartFrame")
+    //     .attr("width", chartInnerWidth)
+    //     .attr("height", chartInnerHeight)
+    //     .attr("transform", translate);
 
-    //create frame for chart border
-   const chartFrame = chart.append("rect")
-       .attr("class", "chartFrame")
-       .attr("width", chartInnerWidth)
-       .attr("height", chartInnerHeight)
-       .attr("transform", translate);
+    //set bars for each country
 
-        //set bars for each country
     var bars = chart.selectAll(".bars")
-        .data(malaria)
-        .enter()
-        .append("rect")
-        .sort((a, b)=> b[expressed]-a[expressed])
-        .attr("class", d =>  "bar " +"co"+ d.Code)
-        .attr("width", chartInnerWidth / malaria.length - 1)
-        .on("mouseover", highlight)
-        .on("mouseout", dehighlight)
-        .on("mousemove", moveLabel);
+    .data(malaria)
+    .enter().filter(d => d[expressed]>0)
+    .append("rect")
+    .sort((a, b)=> b[expressed]-a[expressed])
+    .attr("class", d =>  "bar " +"co"+ d.Code)
+    .attr("width", chartInnerWidth / malaria.length-1)
+    .on("mouseover", highlight)
+    .on("mouseout", dehighlight)
+    .on("mousemove", moveLabel);
 
-        let desc = bars.append("desc")
-        .text('{"stroke": "none", "stroke-width": "0px"}');
+    let desc = bars.append("desc")
+    .text('{"stroke": "none", "stroke-width": "0px"}');
 
-        //below Example 2.8...create a text element for the chart title
-        var chartTitle = chart.append("text")
-                .attr("x", 20)
-                .attr("y", 40)
-                .attr("class", "chartTitle")
-                .text(attrFull[expressed])
-                .attr("transform", translate);
+    //below Example 2.8...create a text element for the chart title
+    var chartTitle = d3.select("#graph").append("text")
+    .attr("x", 20)
+    .attr("y", 40)
+    .attr("class", "chartTitle")
+    .text(attrFull[expressed])
+    .attr("transform", translate);
 
-        updateChart(bars, malaria.length, colorScale);
-};
-//function to create a dropdown menu for attribute selection
-function createDropdown(malaria){
+    updateChart(bars, malaria.length, colorScale);
+  };
+  //function to create a dropdown menu for attribute selection
+  function createDropdown(malaria){
     //add select element
-    const dropdown = d3.select("body")
-        .append("select")
-        .attr("class", "dropdown")
-        .on("change", function(){
-            changeAttribute(this.value, malaria);
-        });
+    const dropdown = d3.select("main")
+    .append("select")
+    .attr("class", "dropdown")
+    .on("change", function(){
+      changeAttribute(this.value, malaria);
+    });
 
     //add initial option
     const titleOption = dropdown.append("option")
-        .attr("class", "titleOption")
-        .attr("disabled", "true")
-        .text("Select Attribute");
+    .attr("class", "titleOption")
+    .attr("disabled", "true")
+    .text("Select Attribute");
 
     //add attribute name options
     const attrOptions = dropdown.selectAll("attrOptions")
-        .data(attributes)
-        .enter()
-        .append("option")
-        .attr("value", d => d )
-        .text(d => d );
-};
+    .data(attributes)
+    .enter()
+    .append("option")
+    .attr("value", d => d )
+    .text(d => d );
+  };
 
-//dropdown change listener handler
-function changeAttribute(attribute, malaria){
+  //dropdown change listener handler
+  function changeAttribute(attribute, malaria){
     //change the expressed attribute
     expressed = attribute;
 
@@ -220,22 +227,22 @@ function changeAttribute(attribute, malaria){
 
     //recolor enumeration units
     const countries = d3.selectAll(".countries")
-        .transition()
-        .duration(500)
-        .style("fill", d => choropleth(d.properties, colorScale));
+    .transition()
+    .duration(500)
+    .style("fill", d => choropleth(d.properties, colorScale));
 
-        //re-sort, resize, and recolor bars
+    //re-sort, resize, and recolor bars
     var bars = d3.selectAll(".bar")
-        .sort((a, b)=> b[expressed]-a[expressed])
-        .transition() //add animation
-        .delay((d, i) =>i * 20)
-        .duration(500);
+    .sort((a, b)=> b[expressed]-a[expressed])
+    .transition() //add animation
+    .delay((d, i) =>i * 20)
+    .duration(500);
 
 
-        updateChart(bars, malaria.length, colorScale)
-};
+    updateChart(bars, malaria.length, colorScale)
+  };
 
-function updateChart(bars, n, colorScale){
+  function updateChart(bars, n, colorScale){
     //position bars
     bars.attr("x", (d, i) => i * (chartInnerWidth / n)+ leftPadding)
     .attr("height", d => chartHeight - yScale(parseFloat(d[expressed]))-10)
@@ -243,61 +250,61 @@ function updateChart(bars, n, colorScale){
     .style('fill', d => choropleth(d, colorScale));
 
     d3.selectAll("g.axis")
-      .call(yAxis);
+    .call(yAxis);
 
     d3.selectAll(".chartTitle")
-        .text(attrFull[expressed]);
-};
+    .text(attrFull[expressed]);
+  };
 
-//function to highlight enumeration units and bars
-function highlight(props){
+  //function to highlight enumeration units and bars
+  function highlight(props){
 
-   //change stroke
-   var selected = d3.selectAll('.co' + props.Code)
-       .style("stroke", "blue")
-       .style("stroke-width", "2");
+    //change stroke
+    var selected = d3.selectAll('.co' + props.Code)
+    .style("stroke", "blue")
+    .style("stroke-width", "2");
 
-       setLabel(props);
+    setLabel(props);
 
-};
+  };
 
-//function to reset the element style
-function dehighlight(props){
-   var selected = d3.selectAll(".co" + props.Code)
-   //used from lecture because for some reason arrow function didn't work in map
-   .style("stroke", function(){
-         return getStyle(this, "stroke")
-     })
-     .style("stroke-width", function(){
-         return getStyle(this, "stroke-width")
-     });
+  //function to reset the element style
+  function dehighlight(props){
+    var selected = d3.selectAll(".co" + props.Code)
+    //used from lecture because for some reason arrow function didn't work in map
+    .style("stroke", function(){
+      return getStyle(this, "stroke")
+    })
+    .style("stroke-width", function(){
+      return getStyle(this, "stroke-width")
+    });
 
-   function getStyle(element, styleName){
-       var styleText = d3.select(element)
-           .select("desc")
-           .text();
+    function getStyle(element, styleName){
+      var styleText = d3.select(element)
+      .select("desc")
+      .text();
 
-       var styleObject = JSON.parse(styleText);
+      var styleObject = JSON.parse(styleText);
 
-       return styleObject[styleName];
-   };
-   d3.select(".infolabel")
-     .remove();
-};
+      return styleObject[styleName];
+    };
+    d3.select(".infolabel")
+    .remove();
+  };
 
 
-function moveLabel(){
+  function moveLabel(){
     //get width of label
     var labelWidth = d3.select(".infolabel")
-        .node()
-        .getBoundingClientRect()
-        .width;
+    .node()
+    .getBoundingClientRect()
+    .width;
 
     //use coordinates of mousemove event to set label coordinates
     var x1 = d3.event.clientX + 10,
-        y1 = d3.event.clientY - 75,
-        x2 = d3.event.clientX - labelWidth - 10,
-        y2 = d3.event.clientY + 25;
+    y1 = d3.event.clientY - 75,
+    x2 = d3.event.clientX - labelWidth - 10,
+    y2 = d3.event.clientY + 25;
 
     //horizontal label coordinate, testing for overflow
     var x = d3.event.clientX > window.innerWidth - labelWidth - 20 ? x2 : x1;
@@ -305,26 +312,35 @@ function moveLabel(){
     var y = d3.event.clientY < 75 ? y2 : y1;
 
     d3.select(".infolabel")
-        .style("left", x + "px")
-        .style("top", y + "px");
-};
-//function to create dynamic label
-function setLabel(props){
+    .style("left", x + "px")
+    .style("top", y + "px");
+  };
+  //function to create dynamic label
+  function setLabel(props){
     //label content
-    var labelAttribute = "<h1>" + props[expressed] +
-        "</h1><b>" + expressed + "</b>";
+    let labelAttribute = "";
+    if (isNaN(props[expressed])){
+      labelAttribute = "<h1>No Available Data</h1>";
+    } else if (expressed =='bedNets' || expressed=='treatment'){
+
+      labelAttribute = "<h1>" + props[expressed] +
+      "%</h1><b>" + attrPop[expressed] + "</b>";
+    } else{
+      labelAttribute = "<h1>" + props[expressed] +
+      "</h1><b>" + attrPop[expressed] + "</b>";
+    }
 
     //create info label div
-    var infolabel = d3.select("body")
-        .append("div")
-        .attr("class", "infolabel")
-        .attr("id", "co" + props.Code + "_label")
-        .html(labelAttribute);
+    var infolabel = d3.select("main")
+    .append("div")
+    .attr("class", "infolabel")
+    .attr("id", "co" + props.Code + "_label")
+    .html(labelAttribute);
 
     var countryName = infolabel.append("div")
-        .attr("class", "labelname")
-        .html(props.Country);
-};
+    .attr("class", "labelname")
+    .html(props.Country);
+  };
   //set up map
   function setMap(){
     //map dimensions
@@ -332,7 +348,7 @@ function setLabel(props){
     const height = 560;
 
     //map container
-    const map = d3.select("body")
+    const map = d3.select("#charts")
     .append("svg")
     .attr("class", "map")
     .attr("width", width)
@@ -357,7 +373,9 @@ function setLabel(props){
     .await(ready);
 
     function ready(error, malaria, africa){
-      createDropdown(malaria);
+      let filteredmalaria = malaria.filter(d=> d['malariaCases']>0);
+      console.log(filteredmalaria);
+      createDropdown(filteredmalaria);
       //convert topojson into geojson
       const africanCountries = topojson.feature(africa, africa.objects.Africa).features;
 
@@ -371,7 +389,7 @@ function setLabel(props){
       //set up distortion lines
       //setGraticule(map, path);
 
-      setChart(malaria, colorScale);
+      setChart(filteredmalaria, colorScale);
     };
   };
 })();
